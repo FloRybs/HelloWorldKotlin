91b4d015297d332c8e893b846e4f1aea742eea8d
Mafraw <marcin.weremczuk1@gmail.com>
2018-01-28 01:55:18 +0100

    2Comit

--- src/pl/main.kt
+++ src/pl/main.kt
@@ -3,5 +3,6 @@ package pl
 fun main(args : Array<String>) {
 
     var aaa = nowaKlasa()
+    aaa.setZmienna1(9999)
     aaa.s()
 }
--- src/pl/nowaKlasa.kt
+++ src/pl/nowaKlasa.kt
@@ -2,8 +2,16 @@ package pl
 
 class nowaKlasa {
 
+    var zmienna1: Int = 1
+
+    fun setZmienna1(c: Int): Int {
+        this.zmienna1 = c
+        println("zmienna 1 ustawiona na "+c)
+        return 0
+    }
     fun s(){
         println("Hello, world!")
         println("Hello, world2!")
+        println("Zmienna 1 ustawiona była na: " + this.zmienna1 +" Dobranoc!!")
     }
 }
